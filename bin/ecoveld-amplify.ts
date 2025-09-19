1f89ab30123a03b92efe0297210d222bf65821a5
ecoveld-dev <ecoveld.tech@gmail.com>
2025-09-18 19:40:50 -0700

    Fixing platform to run ssr

--- bin/ecoveld-amplify.ts
+++ bin/ecoveld-amplify.ts
@@ -3,6 +3,6 @@ import * as cdk from 'aws-cdk-lib';
 import { EcoveldAmplifyStack } from '../lib/ecoveld-amplify-stack';
 
 const app = new cdk.App();
-new EcoveldAmplifyStack(app, 'EcoveldAmplifyStack', {
+new EcoveldAmplifyStack(app, 'EcoveldAmplifyStackCDK', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
--- lib/ecoveld-amplify-stack.ts
+++ lib/ecoveld-amplify-stack.ts
@@ -11,23 +11,23 @@ export class EcoveldAmplifyStack extends cdk.Stack {
 
     const amplifyYml = `
 version: 1
-applications:
-  - frontend:
-      phases:
-        preBuild:
-          commands:
-            - npm ci
-        build:
-          commands:
-            - npm run build
-      artifacts:
-        baseDirectory: .next
-        files:
-          - '**/*'
-      cache:
-        paths:
-          - node_modules/**/*
-    `;
+frontend:
+  phases:
+    preBuild:
+      commands:
+        - npm ci
+    build:
+      commands:
+        - npm run build
+  artifacts:
+    baseDirectory: .next
+    files:
+      - '**/*'
+  cache:
+    paths:
+      - node_modules/**/*
+
+`;
 
     const envVars: amplify.CfnApp.EnvironmentVariableProperty[] = [
       { name: 'NEXT_PUBLIC_API_BASE', value: 'https://99tenhsttd.execute-api.us-west-2.amazonaws.com' },
@@ -38,8 +38,9 @@ applications:
     const app = new amplify.CfnApp(this, 'AmplifyApp', {
       name: 'ecoveld-ui',
       repository,
-      oauthToken: cdk.SecretValue.secretsManager(githubTokenName).unsafeUnwrap(),
       buildSpec: amplifyYml,
+      oauthToken: cdk.SecretValue.secretsManager(githubTokenName).unsafeUnwrap(),
+      platform: "WEB_COMPUTE",
       environmentVariables: envVars
     });
 
@@ -48,7 +49,7 @@ applications:
       branchName: 'main',
       enableAutoBuild: true,
       stage: 'PRODUCTION',
-      framework: 'Next.js - SSR',
+      framework: 'Next.js - SSR',    
       environmentVariables: envVars
     });
     mainBranch.addDependency(app);
